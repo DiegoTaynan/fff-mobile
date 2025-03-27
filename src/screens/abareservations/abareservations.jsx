52543cdf7aa00ca0c23ab5ca87d082cf131b4d10
DiegoTaynan <diegotaynan13@gmail.com>
2025-03-27 11:51:35 -0300

    Branch para correções de erros

--- src/screens/abareservations/abareservations.jsx
+++ src/screens/abareservations/abareservations.jsx
@@ -1,4 +1,4 @@
-import { Alert, FlatList, View } from "react-native";
+import { Alert, FlatList, View, Text } from "react-native";
 import { styles } from "./abareservations.style.js";
 //import { appointments } from "../../constants/dados.js";
 import Appointments from "../../components/appointment/appointment.jsx";
@@ -12,7 +12,10 @@ function AbaReservations() {
     try {
       const response = await api.get("/appointments");
 
-      if (response.data) setAppointments(response.data);
+      if (response.data) {
+        // Verifica se a resposta contém a lista de agendamentos
+        setAppointments(response.data.appointments || response.data);
+      }
     } catch (error) {
       if (error.response?.data.error) Alert.alert(error.response.data.error);
       else Alert.alert("An error has occurred. Please try again later.");
@@ -36,24 +39,28 @@ function AbaReservations() {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={appointments}
-        keyExtractor={(appoint) => appoint.id_appointment}
-        showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => {
-          return (
-            <Appointments
-              id_appointment={item.id_appointment}
-              service={item.service}
-              mechanic={item.mechanic}
-              specialty={item.specialty}
-              bookingDate={item.booking_date}
-              bookingHour={item.booking_hour}
-              onPress={DeleteAppointments}
-            />
-          );
-        }}
-      />
+      {appointments.length === 0 ? (
+        <Text>Nenhum agendamento encontrado</Text>
+      ) : (
+        <FlatList
+          data={appointments}
+          keyExtractor={(appoint) => appoint.id_appointment.toString()}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item }) => {
+            return (
+              <Appointments
+                id_appointment={item.id_appointment}
+                service={item.service}
+                mechanic={item.mechanic}
+                specialty={item.specialty}
+                bookingDate={item.booking_date}
+                bookingHour={item.booking_hour}
+                onPress={DeleteAppointments}
+              />
+            );
+          }}
+        />
+      )}
     </View>
   );
 }
