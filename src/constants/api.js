74e9f7e729d71e14ae189253ce0ec6c751d8ebfe
DiegoTaynan <diegotaynan13@gmail.com>
2025-04-24 14:35:16 -0300

    Testes de conexões

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-18-118-227-83.us-east-2.compute.amazonaws.com:3001", // Atualizado para o endereço público da AWS EC
+  //baseURL: "http://ec2-18-118-227-83.us-east-2.compute.amazonaws.com:3001", // Atualizado para o endereço público da AWS EC
+  baseURL: "http://10.62.17.9:3001",
 });
 
 export default api;
