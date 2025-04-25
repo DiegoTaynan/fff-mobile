eeb8b16d3b44e5473ecb899b7d7c843b9a6cbae0
DiegoTaynan <diegotaynan13@gmail.com>
2025-04-24 23:23:47 -0300

    Endreço atualizado

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-18-118-227-83.us-east-2.compute.amazonaws.com:3001", // Atualizado para o endereço público da AWS EC
+  baseURL: "http://18.118.227.83:3001", // Atualizado para o endereço público da AWS EC
 });
 
 export default api;
