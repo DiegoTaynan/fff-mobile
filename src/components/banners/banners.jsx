b3bacf2820f85dca9e0aaabc1ce47b59620c506b
DiegoTaynan <diegotaynan13@gmail.com>
2025-05-02 18:01:10 -0300

    Implementação é melhorias de todo o aplicativo sem a necessidade de login e uso dos banners no Bucket S3

--- src/components/banners/banners.jsx
+++ src/components/banners/banners.jsx
@@ -1,20 +1,71 @@
-import React from "react";
-import { Image, ScrollView, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  TouchableOpacity,
+  View,
+  Modal,
+  Image,
+  Text,
+} from "react-native";
 import { styles } from "./banners.style";
 
 function Banners(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedBannerIndex, setSelectedBannerIndex] = useState(0);
+
+  const openModal = (index) => {
+    setSelectedBannerIndex(index);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const nextBanner = () => {
+    setSelectedBannerIndex((prevIndex) => (prevIndex + 1) % props.dados.length);
+  };
+
+  const prevBanner = () => {
+    setSelectedBannerIndex(
+      (prevIndex) => (prevIndex - 1 + props.dados.length) % props.dados.length
+    );
+  };
+
   return (
     <View>
       <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        {props.dados.map((banner) => (
+        {props.dados.map((banner, index) => (
           <View key={banner.id_banner} style={styles.banners}>
-            <TouchableOpacity>
-              {/* Use banner.imagePath */}
+            <TouchableOpacity onPress={() => openModal(index)}>
               <Image style={styles.icone} source={{ uri: banner.imagePath }} />
             </TouchableOpacity>
           </View>
         ))}
       </ScrollView>
+
+      {modalVisible && (
+        <Modal visible={modalVisible} transparent={true}>
+          <View style={styles.modalContainer}>
+            <TouchableOpacity style={styles.navButtonLeft} onPress={prevBanner}>
+              <Text style={styles.navButtonText}>{"<"}</Text>
+            </TouchableOpacity>
+            <Image
+              style={styles.modalImage}
+              source={{ uri: props.dados[selectedBannerIndex].imagePath }}
+            />
+            <TouchableOpacity
+              style={styles.navButtonRight}
+              onPress={nextBanner}
+            >
+              <Text style={styles.navButtonText}>{">"}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.closeButton} onPress={closeModal}>
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </Modal>
+      )}
     </View>
   );
 }
--- src/components/banners/banners.style.js
+++ src/components/banners/banners.style.js
@@ -1,6 +1,7 @@
 import { COLORS, FONT_SIZE } from "../../constants/theme";
+import { StyleSheet } from "react-native";
 
-export const styles = {
+export const styles = StyleSheet.create({
   icone: {
     width: 220,
     height: 120,
@@ -8,7 +9,7 @@ export const styles = {
     marginRight: 20,
     borderRadius: 6,
   },
-  banner: {
+  banners: {
     paddingRight: 10,
     marginTop: 20,
     marginBottom: 15,
@@ -18,4 +19,62 @@ export const styles = {
     color: COLORS.gray2,
     textAlign: "center",
   },
-};
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  modalContent: {
+    width: "90%",
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  closeButton: {
+    position: "absolute", // Torna o botão posicionado de forma absoluta
+    bottom: 20, // Ajusta a distância do botão em relação à parte inferior
+    alignSelf: "center", // Centraliza horizontalmente
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+  },
+  closeButtonText: {
+    color: "#000",
+    fontWeight: "bold",
+  },
+  navButtonLeft: {
+    position: "absolute",
+    left: 10,
+    top: "50%",
+    transform: [{ translateY: -20 }],
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 20,
+    padding: 10,
+    zIndex: 10, // Garante que o botão fique acima de outros elementos
+  },
+  navButtonRight: {
+    position: "absolute",
+    right: 10,
+    top: "50%",
+    transform: [{ translateY: -20 }],
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    borderRadius: 20,
+    padding: 10,
+    zIndex: 10, // Garante que o botão fique acima de outros elementos
+  },
+  navButtonText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#000",
+  },
+});
